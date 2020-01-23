7aa0a88decaf0947b4e1b517755b7dd6cd752e2c
Yechen Wei <yew019@ucsd.edu>
2020-01-23 10:07:12 -0800

    ahaha

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,23 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is connected, or is it?");
+		$("#testjs").text("LMAO");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).fadeToggle();
+    }
 }
